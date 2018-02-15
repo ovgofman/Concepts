0b2bf9108da59577ca70272f9a053df47757fab6
hofa <o.v.gofman@gmail.com>
2018-02-15 23:48:02 +0200

    [equations]

---
+++ src/main/java/exercises/equations/eq007.java
@@ -0,0 +1,17 @@
+package exercises.equations;
+
+/**
+ * Created by hofa on 15.02.2018.
+ */
+
+public class eq007 {
+    public static void main(String[] args) {
+        int x = 63;
+        int y = 17;
+
+        int result = x / y;
+        int reminder = x % y;
+
+        System.out.println(x + " / " + y + " = " + result + " and " + reminder + " in a reminder");
+    }
+}
