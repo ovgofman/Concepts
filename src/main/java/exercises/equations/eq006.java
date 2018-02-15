4b5c3c22376c716d871348d30b5672c535bfa698
hofa <o.v.gofman@gmail.com>
2018-02-15 17:24:54 +0200

    [equations]

---
+++ src/main/java/exercises/equations/eq006.java
@@ -0,0 +1,26 @@
+package exercises.equations;
+
+/**
+ * Created by hofa on 15.02.2018.
+ */
+
+public class eq006 {
+    public static void main(String[] args) {
+        double x = 099d;
+
+        if (x >= 100) {
+            double firstDigit = (int) x / 10 / 10;
+            double secondDigit = (int) x / 10 % 10;
+            double thirdDigit = (int) x % 10;
+            System.out.println((int) (firstDigit + secondDigit + thirdDigit));
+        }
+        if (x < 100 && x >= 10) {
+            double firstDigit = (int) x / 10;
+            double secondDigit = (int) x % 10;
+            System.out.println((int) (firstDigit + secondDigit));
+        }
+        if (x < 10) {
+            System.out.println((int) x);
+        }
+    }
+}
