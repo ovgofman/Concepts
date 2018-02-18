f4a278090b7668ee4cf0822e1f22e62e1fc42432
hofa <o.v.gofman@gmail.com>
2018-02-18 16:46:53 +0200

    [equations]

--- src/main/java/exercises/equations/eq016.java
+++ src/main/java/exercises/equations/eq016.java
@@ -8,43 +8,50 @@ public class eq016 {
     public static void main(String[] args) {
         int seconds = 60;
         int minutes = 60;
+        int start = 9 * minutes * seconds;
         int finish = 17 * minutes * seconds;
 
-        String currentTime = "9.00";  // <-- Change the time here
+        String currentTime = "11.59";  // <-- Change the time here
 
         String time[] = currentTime.split("\\.");
-        int h = Integer.valueOf(time[0]);
-        int m = Integer.valueOf(time[1]);
-        int left = ((finish - h * minutes * seconds + m) / minutes / seconds);
+        int currentHours = Integer.valueOf(time[0]);
+        int currentMinutes = Integer.valueOf(time[1]);
+        int currentTimeInSeconds = currentHours * minutes * seconds + currentMinutes * seconds;
 
-        if (left > 0 && left < 9) {
-            switch (left) {
-                case 8:
-                    System.out.println(left + " hours left to 5 p.m.");
-                    break;
-                case 7:
-                    System.out.println(left + " hours left to 5 p.m.");
-                    break;
-                case 6:
-                    System.out.println(left + " hours left to 5 p.m.");
-                    break;
-                case 5:
-                    System.out.println(left + " hours left to 5 p.m.");
-                    break;
-                case 4:
-                    System.out.println(left + " hours left to 5 p.m.");
-                    break;
-                case 3:
-                    System.out.println(left + " hours left to 5 p.m.");
-                    break;
-                case 2:
-                    System.out.println(left + " hours left to 5 p.m.");
-                    break;
-                default:
-                    System.out.println("less than one hour left to 5 p.m.");
-                    break;
-            }
-        } else
+        if (currentTimeInSeconds >= finish || currentTimeInSeconds < start) {
             System.out.println("it's not working time");
+        }
+        if (currentHours < 17 && currentHours >= 16) {
+            System.out.println(finish - currentTimeInSeconds);
+            System.out.println("less than one hour left");
+        }
+        if (currentHours < 16 && currentHours >= 15) {
+            System.out.println(finish - currentTimeInSeconds);
+            System.out.println(((finish - currentTimeInSeconds) / minutes / seconds) + " hours left");
+        }
+        if (currentHours < 15 && currentHours >= 14) {
+            System.out.println(finish - currentTimeInSeconds);
+            System.out.println(((finish - currentTimeInSeconds) / minutes / seconds) + " hours left");
+        }
+        if (currentHours < 14 && currentHours >= 13) {
+            System.out.println(finish - currentTimeInSeconds);
+            System.out.println(((finish - currentTimeInSeconds) / minutes / seconds) + " hours left");
+        }
+        if (currentHours < 13 && currentHours >= 12) {
+            System.out.println(finish - currentTimeInSeconds);
+            System.out.println(((finish - currentTimeInSeconds) / minutes / seconds) + " hours left");
+        }
+        if (currentHours < 12 && currentHours >= 11) {
+            System.out.println(finish - currentTimeInSeconds);
+            System.out.println(((finish - currentTimeInSeconds) / minutes / seconds) + " hours left");
+        }
+        if (currentHours < 11 && currentHours >= 10) {
+            System.out.println(finish - currentTimeInSeconds);
+            System.out.println(((finish - currentTimeInSeconds) / minutes / seconds) + " hours left");
+        }
+        if (currentHours < 10 && currentHours >= 9) {
+            System.out.println(finish - currentTimeInSeconds);
+            System.out.println(((finish - currentTimeInSeconds) / minutes / seconds) + " hours left");
+        }
     }
 }
