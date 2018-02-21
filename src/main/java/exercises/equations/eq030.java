bd4f9cd4597594c7f1ba1bed42df2b4e941ad058
hofa <o.v.gofman@gmail.com>
2018-02-21 23:58:19 +0200

    [equations]

--- src/main/java/exercises/equations/eq030.java
+++ src/main/java/exercises/equations/eq030.java
@@ -8,7 +8,6 @@ import java.io.IOException;
 
 public class eq030 {
     public static void main(String[] args) throws IOException {
-//        Создайте программу, выводящую на экран 12 первых элементов последовательности 2an-2–2, где a1=3 и a2=2.
         for (int i1 = 3, i2 = 2; i1 < 15; i1++, i2++) {
             int first = 2 * (i1 - 2) - 2;
             int second = 2 * (i2 - 2) - 2;
---
+++ src/main/java/exercises/equations/eq031.java
@@ -0,0 +1,16 @@
+package exercises.equations;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Created by hofa on 15.02.2018.
+ */
+
+public class eq031 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+}
