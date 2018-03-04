e5124af51614af86c332296b07c9bbe3a20e0e9f
hofa <o.v.gofman@gmail.com>
2018-03-04 23:53:42 +0200

    [methods]

---
+++ src/main/java/exercises/methods/meth003.java
@@ -0,0 +1,34 @@
+package exercises.methods;
+
+import java.util.Arrays;
+import java.util.Random;
+
+/**
+ * Created by hofa on 02.03.2018.
+ */
+public class meth003 {
+    public static void main(String[] args) {
+        int arr[] = new int[5];
+        for (int i = 0; i < 5; i++) {
+            fillInAnArray(arr);
+            System.out.println(Arrays.toString(arr));
+        }
+    }
+
+    public static int[] sortASC(int[] sortArray) {
+if (sortArray[i]>)
+    }
+
+    public static int[] fillInAnArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = createNumber(3, 10);
+        }
+        return arr;
+    }
+
+    public static int createNumber(int from, int to) {
+        Random random = new Random();
+        int temp = from + random.nextInt(to - from);
+        return temp;
+    }
+}
